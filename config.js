f31c8c927402f6885c76f82b33147b4aa70d500b
buihaiau <106517295+buihaiau@users.noreply.github.com>
2022-05-30 17:01:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,18 +2,18 @@ const CONFIG = {
     titleWeb: "VAY TIEN confess",
     introTitle: 'Who are you',
     introDesc: `Trái đất vốn lạ thường
-    BẠN THÂN MẾN. CHO MÌNH VAY TIỀN NHÉ
-    Đằng sau chữ yêu đây là thương`,
+    BẠN THÂN MẾN. TRẢ NỢ CHO MÌNH NHÉ, 
+    ,
     btnIntro: '^^HiHi^^',
-    title: 'cho anh vay tiền đi 🥰',
+    title: 'CÓ TRẢ CHO BỐ KHÔNG THÌ BẢO',
     desc: '',
     btnYes: 'có nha <33',
     btnNo: 'Không nha :3',
     question: 'Cho tớ biết lí do đi',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'cho thì cho mà không cho thì cho',
+    reply: 'TRẢ thì TRẢ mà không TRẢ THÌ TRẢ',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h, mình qua lấy tiền nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/groups/tek4.vn/'
 }
