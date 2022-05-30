e0b5aa025056ff07cba96e367e28fc27073b8030
buihaiau <106517295+buihaiau@users.noreply.github.com>
2022-05-30 16:49:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
-    titleWeb: "Love confess",
+    titleWeb: "VAY TIEN confess",
     introTitle: 'Who are you',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    BẠN THÂN MẾN. CHO MÌNH VAY TIỀN NHÉ
     Đằng sau chữ yêu đây là thương`,
     btnIntro: '^^HiHi^^',
-    title: 'Làm người yêu tớ nha 🥰',
+    title: 'cho anh vay tiền đi 🥰',
     desc: '',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'có nha <33',
     btnNo: 'Không nha :3',
     question: 'Cho tớ biết lí do đi',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu',
+    reply: 'cho thì cho mà không cho thì cho',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
